bd8c2241e16fa7a01ea335da43a967d3d64259bb
waveofmymind <sjun0913@gmail.com>
2023-05-22 17:04:13 +0900

    Init Project

---
+++ goodjob/src/main/java/com/goodjob/GoodjobApplication.java
@@ -0,0 +1,13 @@
+package com.goodjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodjobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodjobApplication.class, args);
+    }
+
+}
---
+++ goodjob/src/test/java/com/goodjob/GoodjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodjobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
