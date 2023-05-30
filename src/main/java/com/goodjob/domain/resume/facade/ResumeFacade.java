9b33682c05a3c57a9f1302f19677ad42694d8cc4
waveofmymind <sjun0913@gmail.com>
2023-05-30 15:35:36 +0900

    Fix: 필요하지 않은 import 제거

--- src/main/java/com/goodjob/domain/resume/facade/ResumeFacade.java
+++ src/main/java/com/goodjob/domain/resume/facade/ResumeFacade.java
@@ -1,21 +1,15 @@
 package com.goodjob.domain.resume.facade;
 
 import com.goodjob.domain.gpt.GptService;
-import com.goodjob.domain.prompt.constant.PromptType;
 import com.goodjob.domain.resume.dto.request.CreatePromptRequest;
-import com.goodjob.domain.resume.dto.request.GenerateExpectedQuestionRequest;
 import com.goodjob.domain.resume.dto.request.ResumeRequest;
 import com.goodjob.domain.resume.dto.response.WhatGeneratedResponse;
 import com.goodjob.domain.resume.mapper.GenerateExpectedMapper;
-import com.theokanning.openai.completion.chat.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
