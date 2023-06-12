c59772eca76b70780d361036b30380ece3bc82d0
BakChanGyu <qmrma987@gmail.com>
2023-06-12 13:07:41 +0900

    Feat: 깃허브 로그인 기능 추가

--- src/main/java/com/goodjob/global/config/RedisRepositoryConfig.java
+++ src/main/java/com/goodjob/global/config/RedisRepositoryConfig.java
@@ -1,14 +1,11 @@
 package com.goodjob.global.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
