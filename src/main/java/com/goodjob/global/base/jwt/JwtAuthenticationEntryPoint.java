27ffd4c612efa6143e56bd484d4ed18a8632a2b5
BakChanGyu <qmrma987@gmail.com>
2023-06-13 14:17:02 +0900

    Fix: sendRedirect시 http로 넘어가는 문제 수정

--- src/main/java/com/goodjob/global/base/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/goodjob/global/base/jwt/JwtAuthenticationEntryPoint.java
@@ -3,7 +3,6 @@ package com.goodjob.global.base.jwt;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
