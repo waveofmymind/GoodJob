23b7e07ccad295172c0667e4cd8e5ac0a6b426bb
waveofmymind <sjun0913@gmail.com>
2023-06-19 23:11:32 +0900

    Feat: 변환 메서드 추가

--- src/main/java/com/goodjob/domain/resume/dto/response/WhatGeneratedQuestionResponse.java
+++ src/main/java/com/goodjob/domain/resume/dto/response/WhatGeneratedQuestionResponse.java
@@ -1,6 +1,10 @@
 package com.goodjob.domain.resume.dto.response;
 
 
+import com.goodjob.domain.resume.domain.Answers;
+import com.goodjob.domain.resume.domain.ExpectedQuestion;
+import com.goodjob.domain.resume.domain.Questions;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,4 +15,25 @@ public record WhatGeneratedQuestionResponse(
     public WhatGeneratedQuestionResponse() {
         this(new ArrayList<>());
     }
+
+    public ExpectedQuestion toExpectedQuestion() {
+        // Questions와 Answers 객체를 생성
+        List<String> questionsList = new ArrayList<>();
+        List<String> answersList = new ArrayList<>();
+        for (PredictionResponse response : predictionResponse) {
+            questionsList.add(response.question());
+            answersList.add(response.bestAnswer());
+        }
+        Questions questions = Questions.of(questionsList);
+        questions.getQuestions().addAll(questionsList);
+
+        Answers answers = Answers.of(answersList);
+        answers.getAnswers().addAll(answersList);
+
+        return ExpectedQuestion.builder()
+                .questions(questions)
+                .answers(answers)
+                .build();
+    }
 }
+
