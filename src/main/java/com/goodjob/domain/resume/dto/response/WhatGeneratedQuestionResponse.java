7b117939e4b29d1a8e782723b8fe7773b658b2c3
waveofmymind <sjun0913@gmail.com>
2023-06-19 23:16:04 +0900

    Refactor: 로직 개선

--- src/main/java/com/goodjob/domain/resume/dto/response/WhatGeneratedQuestionResponse.java
+++ src/main/java/com/goodjob/domain/resume/dto/response/WhatGeneratedQuestionResponse.java
@@ -7,6 +7,7 @@ import com.goodjob.domain.resume.domain.Questions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record WhatGeneratedQuestionResponse(
         List<PredictionResponse> predictionResponse
@@ -17,23 +18,22 @@ public record WhatGeneratedQuestionResponse(
     }
 
     public ExpectedQuestion toExpectedQuestion() {
-        // Questions와 Answers 객체를 생성
-        List<String> questionsList = new ArrayList<>();
-        List<String> answersList = new ArrayList<>();
-        for (PredictionResponse response : predictionResponse) {
-            questionsList.add(response.question());
-            answersList.add(response.bestAnswer());
-        }
-        Questions questions = Questions.of(questionsList);
-        questions.getQuestions().addAll(questionsList);
+        List<String> questionsList = predictionResponse.stream()
+                .map(PredictionResponse::question)
+                .collect(Collectors.toList());
 
+        List<String> answersList = predictionResponse.stream()
+                .map(PredictionResponse::bestAnswer)
+                .collect(Collectors.toList());
+
+        Questions questions = Questions.of(questionsList);
         Answers answers = Answers.of(answersList);
-        answers.getAnswers().addAll(answersList);
 
         return ExpectedQuestion.builder()
                 .questions(questions)
                 .answers(answers)
                 .build();
     }
+
 }
 
