9d71f446e06c984c30a7cb9e8d001f95fc04e948
waveofmymind <sjun0913@gmail.com>
2023-06-20 11:14:10 +0900

    Feat: 예상 질문 서비스 레이어 추가

---
+++ src/main/java/com/goodjob/domain/resume/repository/ExpectedQuestionJpaRepository.java
@@ -0,0 +1,9 @@
+package com.goodjob.domain.resume.repository;
+
+import com.goodjob.domain.resume.domain.ExpectedQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpectedQuestionJpaRepository extends JpaRepository<ExpectedQuestion,Long> {
+
+
+}
