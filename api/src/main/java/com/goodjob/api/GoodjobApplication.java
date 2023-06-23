7f21e116f5d36126f1bf3934e3985a851e23f92b
waveofmymind <sjun0913@gmail.com>
2023-06-23 20:23:48 +0900

    Feat: gpt모듈 의존성 추가

--- api/src/main/java/com/goodjob/api/GoodjobApplication.java
+++ api/src/main/java/com/goodjob/api/GoodjobApplication.java
@@ -2,6 +2,7 @@ package com.goodjob.api;
 
 import com.goodjob.BatchConfigurationLoader;
 import com.goodjob.core.CoreConfigurationLoader;
+import com.goodjob.gpt.GptConfigurationLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,8 +12,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableCaching
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"com.goodjob.batch.job.repository","com.goodjob.core"})
-@Import({CoreConfigurationLoader.class, BatchConfigurationLoader.class})
+@EnableJpaRepositories(basePackages = {"com.goodjob.batch.job.repository","com.goodjob.core","com.goodjob.gpt"})
+@Import({CoreConfigurationLoader.class, BatchConfigurationLoader.class, GptConfigurationLoader.class})
 public class GoodjobApplication {
     public static void main(String[] args) {
         SpringApplication.run(GoodjobApplication.class, args);
