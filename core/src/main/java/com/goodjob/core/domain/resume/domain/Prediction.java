bba6a83906e0ea6c8ac8585e494d05323e134003
waveofmymind <sjun0913@gmail.com>
2023-06-27 23:59:19 +0900

    Feat: Advice, Question 통일하도록 네이밍 변경

---
+++ core/src/main/java/com/goodjob/core/domain/resume/domain/Prediction.java
@@ -0,0 +1,28 @@
+package com.goodjob.core.domain.resume.domain;
+
+import com.goodjob.core.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+public class Prediction extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long member;
+
+    @Enumerated(EnumType.STRING)
+    private ServiceType serviceType;
+
+    @Embedded
+    private Titles titles;
+
+    @Embedded
+    private Contents contents;
+}
