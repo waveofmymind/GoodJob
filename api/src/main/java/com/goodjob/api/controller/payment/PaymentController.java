40933b2ed442148e85b49593d1ad5deafcd19e99
BakChanGyu <qmrma987@gmail.com>
2023-06-27 18:12:12 +0900

    Feat: 토스 페이먼트 결제 기능

---
+++ api/src/main/java/com/goodjob/api/controller/payment/PaymentController.java
@@ -0,0 +1,83 @@
+package com.goodjob.api.controller.payment;
+
+import com.goodjob.core.domain.payment.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Value("${payment.client-secret}")
+    private String clientSecret;
+
+    @Value("${payment.url}")
+    private String tossUrl;
+
+    private final PaymentService paymentService;
+
+    @GetMapping("/success")
+    public String paymentResult(
+            Model model,
+            @RequestParam(value = "orderId") String orderId,
+            @RequestParam(value = "amount") Integer amount,
+            @RequestParam(value = "paymentKey") String paymentKey) throws Exception {
+
+        HttpURLConnection connection = paymentService.sendPaymentRequest(clientSecret, tossUrl, orderId, amount, paymentKey);
+
+        int code = connection.getResponseCode();
+        boolean isSuccess = code == 200 ? true : false;
+
+        JSONObject jsonObject = paymentService.getPaymentResponse(connection, isSuccess);
+
+        model.addAttribute("isSuccess", isSuccess);
+        model.addAttribute("responseStr", jsonObject.toJSONString());
+        model.addAttribute("method", (String) jsonObject.get("method"));
+        model.addAttribute("orderName", (String) jsonObject.get("orderName"));
+
+        if (((String) jsonObject.get("method")) != null) {
+            if (((String) jsonObject.get("method")).equals("카드")) {
+                model.addAttribute("cardNumber", (String) ((JSONObject) jsonObject.get("card")).get("number"));
+            } else if (((String) jsonObject.get("method")).equals("가상계좌")) {
+                model.addAttribute("accountNumber", (String) ((JSONObject) jsonObject.get("virtualAccount")).get("accountNumber"));
+            } else if (((String) jsonObject.get("method")).equals("계좌이체")) {
+                model.addAttribute("bank", (String) ((JSONObject) jsonObject.get("transfer")).get("bank"));
+            } else if (((String) jsonObject.get("method")).equals("휴대폰")) {
+                model.addAttribute("customerMobilePhone", (String) ((JSONObject) jsonObject.get("mobilePhone")).get("customerMobilePhone"));
+            }
+        } else {
+            model.addAttribute("code", (String) jsonObject.get("code"));
+            model.addAttribute("message", (String) jsonObject.get("message"));
+        }
+
+        return "payment/success";
+    }
+
+    @GetMapping("/fail")
+    public String paymentResult(
+            Model model,
+            @RequestParam(value = "message") String message,
+            @RequestParam(value = "code") Integer code
+    ) throws Exception {
+
+        model.addAttribute("code", code);
+        model.addAttribute("message", message);
+
+        return "payment/fail";
+    }
+
+}
--- core/src/main/generated/com/goodjob/core/domain/hashTag/mapper/HashTagMapperImpl.java
+++ core/src/main/generated/com/goodjob/core/domain/hashTag/mapper/HashTagMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-06-25T19:31:54+0900",
+    date = "2023-06-27T16:31:11+0900",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.6 (Amazon.com Inc.)"
 )
 @Component
