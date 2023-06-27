f8829d95f0364b101c57e42dd53977e2cc12c062
waveofmymind <sjun0913@gmail.com>
2023-06-28 00:01:36 +0900

    Feat: 엔티티 추가 메서드 구현

---
+++ core/src/main/java/com/goodjob/core/domain/resume/domain/Titles.java
@@ -0,0 +1,32 @@
+package com.goodjob.core.domain.resume.domain;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embeddable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Embeddable
+public class Titles {
+    @ElementCollection
+    private List<String> titles;
+
+    protected Titles() {
+        this.titles = new ArrayList<>();
+    }
+
+    public Titles(List<String> questions) {
+        this.titles = new ArrayList<>(questions);
+    }
+
+    public List<String> getQuestions() {
+        return Collections.unmodifiableList(titles);
+    }
+
+    public static Titles of(List<String> questions) {
+        Titles instance = new Titles();
+        instance.titles.addAll(questions);
+        return instance;
+    }
+}
