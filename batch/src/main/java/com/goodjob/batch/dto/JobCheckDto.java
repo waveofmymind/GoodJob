bd9d113b30d0db0b54740e499681b1850545836d
PARKPARKWOO <wy9295@naver.com>
2023-07-01 01:35:08 +0900

    Feat: Batch 분리

--- batch/src/main/java/com/goodjob/batch/dto/JobCheckDto.java
+++
@@ -1,7 +0,0 @@
-package com.goodjob.batch.dto;
-
-public record JobCheckDto(
-        String company, String subject,
-        String url, String sector,
-        int sectorCode, String createDate, int career) {
-}
