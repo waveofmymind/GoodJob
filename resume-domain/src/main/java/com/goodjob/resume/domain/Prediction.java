f75913bb2f830fbcbca949dfe07bf353962c881f
waveofmymind <sjun0913@gmail.com>
2023-07-01 09:27:34 +0900

    Feat: 배치 모듈 삭제로 의존성 다듬기 및 이력서 모듈 BaseEntity 추가

--- resume-domain/src/main/java/com/goodjob/resume/domain/Prediction.java
+++ resume-domain/src/main/java/com/goodjob/resume/domain/Prediction.java
@@ -1,18 +1,15 @@
 package com.goodjob.resume.domain;
 
+import com.goodjob.resume.config.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @AllArgsConstructor
-public class Prediction {
+public class Prediction extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,10 +25,4 @@ public class Prediction {
 
     @Embedded
     private Contents contents;
-
-    @CreatedDate
-    private LocalDateTime createdDate;
-
-    @LastModifiedDate
-    private LocalDateTime lastModifiedDate;
 }
