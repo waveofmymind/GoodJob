b67dd80975009fe0ff201a3057429e92ba27152d
waveofmymind <sjun0913@gmail.com>
2023-07-03 15:01:06 +0900

    Refactor: 운영 토픽 추가

--- resume-domain/src/main/java/com/goodjob/resume/adaptor/KafkaPredictionProducer.java
+++ resume-domain/src/main/java/com/goodjob/resume/adaptor/KafkaPredictionProducer.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class KafkaPredictionProducer {
-    private static final String TOPIC_QUESTION = "question-request";
-    private static final String TOPIC_ADVICE = "advice-request";
+    private static final String TOPIC_QUESTION = "question-request-prod";
+    private static final String TOPIC_ADVICE = "advice-request-prod";
     private final KafkaTemplate<String, String> kafkaTemplate;
     public void sendQuestionRequest(String message) {
         this.kafkaTemplate.send(TOPIC_QUESTION, message);
