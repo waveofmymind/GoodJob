ad33a29332bdb67e009da65c8f75ad2953ccd96f
BakChanGyu <qmrma987@gmail.com>
2023-07-05 18:06:54 +0900

    Feat: 코인 재발급 기능, 코인체크로직 수정

--- core/src/main/java/com/goodjob/core/global/base/coin/CoinUt.java
+++ core/src/main/java/com/goodjob/core/global/base/coin/CoinUt.java
@@ -1,16 +1,22 @@
 package com.goodjob.core.global.base.coin;
 
 import com.goodjob.core.domain.member.entity.Member;
+import com.goodjob.core.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
 public class CoinUt {
 
     public final static int MAX_COIN_COUNT = 10;
+    private final MemberService memberService;
 
-    @Transactional
     public boolean isServiceAvailable(Member member) {
         // 유료 회원인 경우 코인 없이 이용 가능
         if (!member.getUserRole().equals("free")) {
@@ -23,9 +29,15 @@ public class CoinUt {
             return false;
         }
 
-        member.deductCoin(); // 코인감소
+        memberService.deductCoin(member); // 코인 감소
 
         return true;
     }
 
+    @Scheduled(cron = "${scheduler.cron.job}", zone = "Asia/Seoul")
+    public void updateCoins() {
+        log.info("코인 초기화");
+        // 모든 free 회원의 coin을 10으로 초기화
+        memberService.updateCoins();
+    }
 }
