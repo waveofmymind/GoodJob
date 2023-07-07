6f67e60e6e210a0475919325f507bf3bfe757481
waveofmymind <sjun0913@gmail.com>
2023-07-07 19:29:18 +0900

    Feat: 카프카 토픽 프로필 분리

--- resume-domain/src/main/java/com/goodjob/resume/facade/ResumeFacade.java
+++ resume-domain/src/main/java/com/goodjob/resume/facade/ResumeFacade.java
@@ -26,7 +26,7 @@ public class ResumeFacade {
     private final ObjectMapper objectMapper;
     private final SavePredictionUseCase savePredictionUseCase;
 
-    @KafkaListener(topics = "question-request-prod", groupId = "gptgroup")
+    @KafkaListener(topics = "${custom.kafka.topic.question}", groupId = "gptgroup")
     public void generatedQuestionResponseWithKafka(String message) {
         try {
             CreatePromptRequest request = objectMapper.readValue(message, CreatePromptRequest.class);
@@ -47,7 +47,7 @@ public class ResumeFacade {
 
     }
 
-    @KafkaListener(topics = "advice-request-prod", groupId = "gptgroup")
+    @KafkaListener(topics = "${custom.kafka.topic.advice}", groupId = "gptgroup")
     public void generateAdviceWithKafka(String message) {
         try {
             CreatePromptRequest request = objectMapper.readValue(message, CreatePromptRequest.class);
