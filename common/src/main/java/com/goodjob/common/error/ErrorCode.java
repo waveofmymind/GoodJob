b31fe3813af854b781e0878e4fd5ebbe3e57fe17
waveofmymind <sjun0913@gmail.com>
2023-07-19 01:42:07 +0900

    feat: AI 서비스 경로 권한 설정

--- common/src/main/java/com/goodjob/common/error/ErrorCode.java
+++ common/src/main/java/com/goodjob/common/error/ErrorCode.java
@@ -6,12 +6,16 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    CREATE_PREDICTION_QUESTION(HttpStatus.BAD_REQUEST,"예상 질문을 만들 수 없습니다."),
-    THREAD_MALFUNCTION(HttpStatus.INTERNAL_SERVER_ERROR,  "쓰레드 충돌"),
+    CREATE_PREDICTION_QUESTION(HttpStatus.BAD_REQUEST, "예상 질문을 만들 수 없습니다."),
+    THREAD_MALFUNCTION(HttpStatus.INTERNAL_SERVER_ERROR, "쓰레드 충돌"),
     PROMPT_NOT_EXISTS(HttpStatus.NOT_FOUND, "존재하지 않는 프롬프트입니다."),
     JSON_PARSING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 파싱에 실패했습니다."),
 
-    PREDICTION_NOT_FOUND(HttpStatus.NOT_FOUND, "AI 응답 정보가 존재하지 않습니다.");
+    PREDICTION_NOT_FOUND(HttpStatus.NOT_FOUND, "AI 응답 정보가 존재하지 않습니다."),
+
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
+    COIN_NOT_ENOUGH(HttpStatus.BAD_REQUEST, "코인이 부족합니다."),
+    ;
 
 
     ErrorCode(HttpStatus httpStatus, String message) {
