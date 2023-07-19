bbc0a232246b0ea1e652d5cfa92674eb06f3902e
waveofmymind <sjun0913@gmail.com>
2023-07-19 21:22:19 +0900

    feat: 예외 테스트 추가

--- resume-domain/src/test/java/com/goodjob/resume/facade/ResumeFacadeTest.java
+++ resume-domain/src/test/java/com/goodjob/resume/facade/ResumeFacadeTest.java
@@ -2,6 +2,9 @@ package com.goodjob.resume.facade;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.goodjob.common.error.ErrorCode;
+import com.goodjob.common.error.exception.BusinessException;
+import com.goodjob.resume.adaptor.outs.persistence.KafkaPredictionProducer;
 import com.goodjob.resume.dto.request.CreatePromptRequest;
 import com.goodjob.resume.dto.response.ImprovementResponse;
 import com.goodjob.resume.dto.response.PredictionResponse;
@@ -18,6 +21,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.times;
@@ -33,6 +37,9 @@ class ResumeFacadeTest {
 
     @Mock
     private ObjectMapper objectMapper;
+
+    @Mock
+    private KafkaPredictionProducer kafkaPredictionProducer;
 
     @InjectMocks
     private ResumeFacade resumeFacade;
@@ -76,6 +83,45 @@ class ResumeFacadeTest {
 
         // Then
         verify(savePredictionUseCase, times(1)).savePrediction(any());
+    }
+
+    @DisplayName("면접 예상 질문 답변 생성이 실패한 경우 예외를 반환한다.")
+    @Test
+    void testGeneratedQuestionResponseExceptionTest() throws JsonProcessingException {
+        // Given
+        String message = "테스트 메시지";
+        CreatePromptRequest promptRequest = new CreatePromptRequest(1L, "Software backend Developer", "Newcomer", null);
+        WhatGeneratedQuestionResponse mockResponse = new WhatGeneratedQuestionResponse(new ArrayList<>());
+        mockResponse.predictionResponse().add(new PredictionResponse("question", "answer"));
+
+        //when
+        when(objectMapper.readValue(message, CreatePromptRequest.class)).thenReturn(promptRequest);
+
+        when(gptService.createdExpectedQuestionsAndAnswer(any(), any(), any())).thenThrow(new BusinessException(ErrorCode.JSON_PARSING_FAILED));
+
+        // Then
+        assertThrows(BusinessException.class, () -> {
+            resumeFacade.generatedQuestionResponseWithKafka(message);
+        });
+    }
+
+    @DisplayName("이력서 개선안 응답이 실패하면 예외를 반환한다.")
+    @Test
+    void testGeneratedAdviceResponseExceptionTest() throws JsonProcessingException {
+        // Given
+        String message = "테스트 메시지";
+        CreatePromptRequest promptRequest = new CreatePromptRequest(1L, "Software backend Developer", "Newcomer", null);
+        WhatGeneratedImproveResponse mockResponse = new WhatGeneratedImproveResponse(new ArrayList<>());
+        mockResponse.improvementResponse().add(new ImprovementResponse("improvementPoint", "advice"));
+
+        //when
+        when(objectMapper.readValue(message, CreatePromptRequest.class)).thenReturn(promptRequest);
+        when(gptService.createdImprovementPointsAndAdvice(any(), any(), any())).thenThrow(new BusinessException(ErrorCode.JSON_PARSING_FAILED));
+
+        // Then
+        assertThrows(BusinessException.class, () -> {
+            resumeFacade.generateAdviceWithKafka(message);
+        });
     }
 
 
