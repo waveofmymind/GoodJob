6637c4b04214565e39201518eb837c604685cdf7
waveofmymind <sjun0913@gmail.com>
2023-07-28 02:18:28 +0900

    fix: LocalDateTime 직렬화 오류 해결을 위한 Config 추가

---
+++ api/src/main/java/com/goodjob/api/feign/FeignConfig.java
@@ -0,0 +1,19 @@
+package com.goodjob.api.feign;
+
+import org.springframework.cloud.openfeign.FeignFormatterRegistrar;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public FeignFormatterRegistrar dateTimeFormatterRegistrar() {
+        return registry -> {
+            DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+            registrar.setUseIsoFormat(true);
+            registrar.registerFormatters(registry);
+        };
+    }
+}
