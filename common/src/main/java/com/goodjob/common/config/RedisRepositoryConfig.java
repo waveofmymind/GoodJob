43f59aae4feb8261eea8a81bac05f5a686c3057e
waveofmymind <sjun0913@gmail.com>
2023-07-31 13:34:34 +0900

    feat: 추후 클러스터드 모드를 위해 RedisConfig 수정 #291

--- common/src/main/java/com/goodjob/common/config/RedisRepositoryConfig.java
+++ common/src/main/java/com/goodjob/common/config/RedisRepositoryConfig.java
@@ -1,12 +1,25 @@
 package com.goodjob.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+import static io.lettuce.core.ReadFrom.REPLICA_PREFERRED;
 
 @Configuration
 public class RedisRepositoryConfig {
@@ -19,20 +32,58 @@ public class RedisRepositoryConfig {
 
     // lettuce
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+    public RedisStandaloneConfiguration redisStandaloneConfiguration() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        return redisStandaloneConfiguration;
     }
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate() {
+    public RedisConnectionFactory redisConnectionFactory(
+            final RedisStandaloneConfiguration redisStandaloneConfiguration) {
+        final SocketOptions socketOptions =
+                SocketOptions.builder().connectTimeout(Duration.of(10, ChronoUnit.MINUTES)).build();
+
+        final var clientOptions =
+                ClientOptions.builder().socketOptions(socketOptions).autoReconnect(true).build();
+
+        var clientConfig =
+                LettuceClientConfiguration.builder()
+                        .clientOptions(clientOptions)
+                        .readFrom(REPLICA_PREFERRED);
+
+
+        return new LettuceConnectionFactory(
+                redisStandaloneConfiguration, clientConfig.build());
+    }
+
+
+    @Primary
+    @Bean
+    public RedisTemplate<String, String> redisStringTemplate() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory(redisStandaloneConfiguration()));
         redisTemplate.setEnableTransactionSupport(true);
 
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+    @Bean
+    public RedisTemplate<String, Object> redisObjectTemplate(ObjectMapper objectMapper) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory(redisStandaloneConfiguration()));
+        redisTemplate.setEnableTransactionSupport(true);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        redisTemplate.setEnableTransactionSupport(true);
 
         return redisTemplate;
     }
