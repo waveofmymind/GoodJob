dc96cfca69b3aa53eb1407db8275295d7be98190
waveofmymind <sjun0913@gmail.com>
2023-07-31 13:32:00 +0900

    feat: 캐싱을 위해 Serializable 구현 #291

--- article-domain/src/main/java/com/goodjob/article/domain/keyword/dto/response/KeyWordResponseDto.java
+++ article-domain/src/main/java/com/goodjob/article/domain/keyword/dto/response/KeyWordResponseDto.java
@@ -3,9 +3,11 @@ package com.goodjob.article.domain.keyword.dto.response;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class KeyWordResponseDto {
+public class KeyWordResponseDto implements Serializable {
     private Long id;
     private String content;
 
